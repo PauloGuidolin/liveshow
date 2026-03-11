06a8aea68dbc80a564d31ac0119627ceaabc8c28
PauloGuidolin <paulog162003@gmail.com>
2026-03-11 12:22:09 -0300

    seccion admin

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+const shows = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    image: z.string(),
+  }),
+});
+
+export const collections = { shows };
